21400522ce32181c1e2982bc3dbce8d33c2090f2
PROFF-1 <richmondnyamedor04@gmail.com>
2025-06-07 01:43:51 +0000

    Welcome screen created with custom logo

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import React from 'react';
 import { DashboardScreen } from './src/screens';
+import WelcomeScreen from './src/screens/WelcomeScreen';
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: '#000' }}>
       <StatusBar style="light" />
-      <DashboardScreen />
+      <WelcomeScreen />
     </View>
   );
 }
